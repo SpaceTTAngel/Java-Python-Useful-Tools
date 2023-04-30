c99611dc9255ddd12b7525f270a2ff9fbe7fa463
SpaceTTAngel <131013386+SpaceTTAngel@users.noreply.github.com>
2023-04-30 18:59:58 +0200

    Add files via upload
    First issue of address to Google Maps coordinates converter

---
+++ Python/Web Scraper/Address_to_GoogleMap_coord_converter.py
@@ -0,0 +1,14 @@
+import googlemaps
+from datetime import datetime
+
+# Inizializza il client della Google Maps API
+gmaps = googlemaps.Client(key='AIzaSyA2dnkiQKQwJN73ZHytMpD8P0CmL5eXOWI')
+
+# Indirizzo di cui si vogliono conoscere le coordinate
+indirizzo = 'via Livia Drusilla, Cinecittà, Roma'
+
+# Utilizza la funzione geocode() per ottenere le coordinate dell'indirizzo
+geocode_result = gmaps.geocode(indirizzo)
+
+# Stampa le coordinate dell'indirizzo
+print(geocode_result[0]['geometry']['location'])
